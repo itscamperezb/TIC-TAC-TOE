823fd85aa37ddd639bf46d9b148e4b645edc8439
Camilo <camilopb0405@gmail.com>
2026-02-18 11:19:58 -0500

    terminado

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 export default function Header() {
   return (
     <header>
-      <img src="../public/game-logo.png"></img>
+      <img src="/game-logo.png"></img>
       <h1>TIC TAC TOE</h1>
     </header>
   );
